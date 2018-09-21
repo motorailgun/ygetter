6c8a601a5361578399fab9548ee80e687e6a3ef0
motorailgun <mtkmtt.cd@gmail.com>
2018-09-21 23:32:53 +0900

    Modified to check the highest resolution

--- ygetter.rb
+++ ygetter.rb
@@ -5,8 +5,8 @@ require "nkf"
 class String	
 	def make_group_by_itag
 		#make groupes related to itag
-		#returnes [["itag","resolution","quality"."url"],[... ]]
-		return self.scan(/itag=(\d+?)&.*?&size=(\d+?x\d+?)&.*?&quality_label=(.+?)&.+?&url=(.+?)&/)
+		#returnes [["url","itag","resolution","quality"],[... ]]
+		return self.scan(/&url=(.+?)&.*?&itag=(\d+?)&.*?&size=(\d+?x\d+?)&.*?&quality_label=(.+?)&/)
 	end
 	
 	def get_video_id
@@ -23,36 +23,48 @@ def getinfo(vid)
 end
 		
 def transaction(url)
-	video_info_raw = getinfo(url.get_video_id)
-	video_info = video_info_raw.make_group_by_itag
-	save_to = NKF.nkf("-m0 -w8",URI.unescape(video_info_raw).scan(/title=(.+?)&/)[0][0].to_s)
-	video_resolution_and_url = [0,""]
-	p save_to
-	
-	video_info.each{|array|
+	begin
 		begin
-			array[1] = array[1].gsub(/x/,"").to_i
-		rescue
-			array[1] = 0
-		end
+		video_info_raw = getinfo(url.get_video_id)
+		video_info = video_info_raw.make_group_by_itag
+		save_to = NKF.nkf("-m0 -w8",URI.unescape(video_info_raw).scan(/title=(.+?)&/)[0][0].to_s)
+		video_resolution_and_url = [0,"",""]
 		
-		if array[1] == 19201080 then
-			video_resolution_and_url = [array[1],array[3]]
-			break
-		elsif array[1] > video_resolution_and_url[0]
-			video_resolution_and_url = [array[1],array[3]]
+			video_info.each{|array|
+				begin
+					array[2] = array[2].gsub(/x/,"").to_i
+				rescue
+					array[2] = 0
+				end
+		
+				if array[2] == 19201080 then
+					video_resolution_and_url = [array[2],array[0],array[3]]
+					break
+				elsif array[2] > video_resolution_and_url[0]
+					video_resolution_and_url = [array[2],array[0],array[3]]
+				end
+			}
+		
+			if video_resolution_and_url[0].to_s.scan(/#{video_resolution_and_url[2].chop}/).empty? then
+				puts video_resolution_and_url
+				raise
+			end
+		rescue
+			sleep(3)
+			retry
 		end
-	}
 	
-	puts("Download started:#{save_to};resolution:#{video_resolution_and_url[0]}")
+		puts("Download started:#{save_to};resolution:#{video_resolution_and_url[0]}")
 	
 	
-	video = open(URI.unescape(video_resolution_and_url[1]))
-	saveto = open("#{save_to[0]}.mp4","a+")
-	bytes = saveto.write(video.read)
-	
-	video.close
-	saveto.close
+		video = open(URI.unescape(video_resolution_and_url[1]))
+		saveto = open("#{save_to}.mp4","a+")
+		bytes = saveto.write(video.read)
+		video.close
+		saveto.close
+	rescue
+		retry
+	end
 	
 	puts("done. #{bytes} bytes saved")
 end
